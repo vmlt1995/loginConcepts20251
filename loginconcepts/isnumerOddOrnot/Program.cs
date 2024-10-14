a07067ad1649a96bb05401de271b57ed2396d762
Laptop <Laptop@WEEPA>
2024-10-14 13:21:39 -0400

    .

---
+++ loginconcepts/isnumerOddOrnot/Program.cs
@@ -0,0 +1,28 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.Write("ingrese numero entero o 'Salir' para salir:");
+     numberString = Console.ReadLine();
+    if (numberString.ToLower() == "salir")
+    {
+        continue;
+    }
+
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"el numero {numberInt}, es par");
+        }
+        else
+        {
+            Console.WriteLine($"el numero {numberInt}, es impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"lo que ingresaste: {numberString}, no es numero entero.");
+    }
+} while (numberString !.ToLower() != "salir");
+Console.WriteLine("game over.");
